2b9461038ce210d0fe64a7581b5ae0377c39e40e
Gary Wenneker <gary.wenneker@macaw.nl>
2019-03-31 09:53:38 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Challenge Accepted, bitch!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            enter BlockChain
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
